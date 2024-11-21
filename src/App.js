dccd27bec21f839b1c996cc1c64d40a0b94e6c57
Bhavil-13 <bhavilsharma83@gmail.com>
2024-11-22 01:50:27 +0530

    first addition to frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+import LoginComponent from './components/LoginComponent';
+import SignupComponent from './components/SignupComponent';
 
 function App() {
   return (
-    <div className="App">
-      hello world, chamanlal on the line
+    <div>
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+        // <Route path="/signup" element={<SignupComponent />} />
+          <Route exact path="/" element={<SignupComponent />} />
+          <Route exact path="/login" element={<LoginComponent />} />
+          <Route path="/employees" element={<ListEmployeeComponent />} />
+          <Route path="*" element={<ListEmployeeComponent />} />
+          <Route path='/add-employee' element={<AddEmployeeComponent/>} />
+          <Route path='/edit-employee/:id' element={<AddEmployeeComponent/>} />
+          <Route path="/login" element={LoginComponent} />
+        </Routes>
+        {/* <ListEmployeeComponent /> */}
+        <FooterComponent />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
+// // App.js
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import FooterComponent from './components/FooterComponent';
+// import HeaderComponent from './components/HeaderComponent';
+// import ListEmployeeComponent from './components/ListEmployeeComponent';
+// import AddEmployeeComponent from './components/AddEmployeeComponent';
+// import LoginComponent from './components/LoginComponent';
+// import SignupComponent from './components/SignupComponent';
+
+// function App() {
+//   return ( 
+//     <div>
+//       <Router>
+//         <HeaderComponent />
+//         <Routes>
+//         <Route path="/signup" element={<SignupComponent />} />
+//           <Route path="/login" element={<LoginComponent />} />
+//           <Route path="/employees" element={<ListEmployeeComponent />} />
+//           <Route path='/add-employee' element={<AddEmployeeComponent />} />
+//           <Route path='/edit-employee/:id' element={<AddEmployeeComponent />} />
+//           <Route path="*" element={<ListEmployeeComponent />} />
+//         </Routes>
+//         <FooterComponent />
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
