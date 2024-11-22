696e4106be8bfbe0971e1134ff2917123a506704
Bhavil-13 <bhavilsharma83@gmail.com>
2024-11-22 10:50:30 +0530

    edit in addemployeecomponent.js

--- src/components/AddEmployeeComponent.js
+++ src/components/AddEmployeeComponent.js
@@ -3,126 +3,113 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AddEmployeeComponent.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AddEmployeeComponent = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [emailId, setEmailId] = useState('');
-  const history = useNavigate();
+  const [employee, setEmployee] = useState({
+    firstName: '',
+    lastName: '',
+    emailId: '',
+  });
+
+  const navigate = useNavigate();
   const { id } = useParams();
 
-  const saveOrUpdateEmployee = (e) => {
-    e.preventDefault();
-
-    const employee = { firstName, lastName, emailId };
-
-    if (id) {
-      EmployeeService.updateEmployee(id, employee)
-        .then((response) => {
-          history('/employees');
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    } else {
-      EmployeeService.createEmployee(employee)
-        .then((response) => {
-          console.log(response.data);
-          history('/employees');
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  };
-
   useEffect(() => {
-    EmployeeService.getEmployeeById(id)
-      .then((response) => {
-        setFirstName(response.data.firstName);
-        setLastName(response.data.lastName);
-        setEmailId(response.data.emailId);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (id) {
+      EmployeeService.getEmployeeById(id)
+        .then((response) => {
+          setEmployee(response.data);
+        })
+        .catch((error) => console.error(error));
+    }
   }, [id]);
 
-  const title = () => {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEmployee((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
     if (id) {
-      return <h2 className="text-center">Update Employee</h2>;
+      EmployeeService.updateEmployee(id, employee)
+        .then(() => navigate('/employees'))
+        .catch((error) => console.error(error));
     } else {
-      return <h2 className="text-center">Add Employee</h2>;
+      EmployeeService.createEmployee(employee)
+        .then(() => navigate('/employees'))
+        .catch((error) => console.error(error));
     }
   };
 
+  const renderTitle = () => (
+    <h2 className="text-center">{id ? 'Update Employee' : 'Add Employee'}</h2>
+  );
+
   return (
-    <div>
-      <br />
-      <br />
-      <div className="container">
-        <div className="row">
-          <div className="card col-md-6 offset-md-3 offset-md-3">
-            {title()}
-            <div className="card-body">
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="firstName" className="form-label">
-                    First Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="firstName"
-                    className="form-control"
-                    placeholder="Enter first name"
-                    name="firstName"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                  />
-                </div>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="card col-md-6 offset-md-3">
+          {renderTitle()}
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="firstName" className="form-label">
+                  First Name:
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  className="form-control"
+                  placeholder="Enter first name"
+                  value={employee.firstName}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
 
-                <div className="mb-3">
-                  <label htmlFor="lastName" className="form-label">
-                    Last Name:
-                  </label>
-                  <input
-                    type="text"
-                    id="lastName"
-                    className="form-control"
-                    placeholder="Enter last name"
-                    name="lastName"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                  />
-                </div>
+              <div className="mb-3">
+                <label htmlFor="lastName" className="form-label">
+                  Last Name:
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  className="form-control"
+                  placeholder="Enter last name"
+                  value={employee.lastName}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
 
-                <div className="mb-3">
-                  <label htmlFor="emailId" className="form-label">
-                    Email Id:
-                  </label>
-                  <input
-                    type="email"
-                    id="emailId"
-                    className="form-control"
-                    placeholder="Enter email Id"
-                    name="emailId"
-                    value={emailId}
-                    onChange={(e) => setEmailId(e.target.value)}
-                  />
-                </div>
+              <div className="mb-3">
+                <label htmlFor="emailId" className="form-label">
+                  Email Id:
+                </label>
+                <input
+                  type="email"
+                  id="emailId"
+                  name="emailId"
+                  className="form-control"
+                  placeholder="Enter email ID"
+                  value={employee.emailId}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
 
-                <button
-                  className="btn btn-success me-2"
-                  onClick={(e) => saveOrUpdateEmployee(e)}
-                >
+              <div className="d-flex justify-content-between">
+                <button type="submit" className="btn btn-success">
                   Submit
                 </button>
                 <Link to="/employees" className="btn btn-danger">
                   Cancel
                 </Link>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
